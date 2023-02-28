28d0e81c50280145308d85fe744a2ff2f31dece1
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-02-28 09:57:13 +0200

    deleted height:100vh in App.jsx to enable scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { FriendList } from './FriendList/FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import transactions from '../data/transactions.json';
 import friends from '../data/friends.json';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
